02a3b291c34ab39cf27d0a9aff82d03289e87f59
Jeremy Mates <jeremy.mates@gmail.com>
2022-11-01 15:35:20 -0700

    display the pieces
     * borrow some openart chess pieces
     * display them on the board
     * various other bits of the game now implemented

--- board.lisp
+++ board.lisp
@@ -15,6 +15,12 @@
 ; TODO probably want sub-packages for /graph and /board
 (in-package :marad)
 
+(defconstant +empty-cell+ 0)
+(defconstant +move-other+ 0)
+(defconstant +move-square+ 1)   ; also, the Pawn type
+(defconstant +move-diagonal+ 2) ; also, the Bishop type
+; King is "3", which is both a Pawn and a Bishop. see how that works?
+
 ; flags
 (defconstant +board-player+ 6)
 (defconstant +board-moved+  7)
@@ -37,13 +43,34 @@
 (defmacro board-get-flag (flag x) `(ldb (byte 1 ,flag) ,x))
 (defmacro board-set-flag (flag x) `(setf (ldb (byte 1 ,flag) ,x) 1))
 
-(defmacro with-gameboard ((game board row col) body)
+(defmacro with-gameboard ((game board row col) &body body)
   (let ((size (gensym)))
     `(let ((,size (gameboard-size game))
            (,board (gameboard-board game)))
        (dotimes (,row ,size)
          (dotimes (,col ,size)
            ,@body)))))
+
+(defmacro with-board-pieces ((game board row col owner type) &body body)
+  (let ((cell (gensym)))
+    `(with-gameboard
+       (,game ,board ,row ,col)
+       (let* ((,cell (aref ,board ,row ,col))
+              (,type (board-get-type ,cell)))
+         (when (plusp ,type)
+           (let ((,owner (board-get-flag +board-player+ ,cell)))
+             ,@body))))))
+
+; this may result in nothing or several things happening
+; uh once it is written, anyways
+(defun do-move (game srcx srcy stepx stepy)
+  (let ((move-count (gameboard-moves game)))
+    )
+  )
+
+(defun cell-details (cell)
+  (values (board-get-type cell)
+          (board-get-flag +board-player+ cell)))
 
 ; call this at the beginning of each turn, and before saving
 (defun clear-moved (game)
@@ -52,13 +79,19 @@
     (setf (ldb (byte 1 +board-moved+)
                (aref board row col)) 0)))
 
-; gameboard in the starting position. "moves" is yet to be determined,
-; and it is the 0th turn. the pieces are vertical as I was thinking of
-; Archon (1983) when playtesting the game with some coins on a
-; cardboard grid
-;
-; NOTE "moves" of zero for a second player turn is an illegal state; the
-; move count must be shared between pairs of turns
+(defun get-cell (game row col)
+  (let ((cell (aref (gameboard-board game) row col)))
+    (when (plusp cell) cell)))
+
+(defun legal-move? (typea typeb)
+  (plusp (logand typea typeb)))
+
+; this is rolled at the beginning of each turnpair
+(defun move-count () (1+ (random 4)))
+
+; a gameboard in the starting position. the pieces are vertical as I was
+; thinking of Archon (1983) when playtesting the game with some coins on
+; a piece of ruled cardboard
 (defun new-gameboard (size)
   (let ((board (make-array `(,size ,size) :element-type '(unsigned-byte 8)
                            :initial-contents '((0 0 0 0 0 0 0 0 0)
@@ -70,15 +103,44 @@
                                                (0 1 0 0 0 0 0 65 0)
                                                (0 2 0 0 0 0 0 66 0)
                                                (0 0 0 0 0 0 0 0 0)))))
-    (make-gameboard
-      :size size :board board
-      :score (make-array 2 :element-type 'fixnum :initial-element 0))))
+    (make-gameboard :size size
+                    :board board
+                    :moves (move-count)
+                    :score (make-array 2 :element-type 'fixnum
+                                       :initial-element 0))))
+
+(defun next-turn (game)
+  (let ((turn (incf (gameboard-turn game))))
+    (when (zerop (mod turn 2))
+      (setf (gameboard-moves game) (move-count)))))
 
 ; did something move in the given cell (probably the central scoring cell)
 (defun moved? (game row col)
   (plusp (ldb (byte 1 +board-moved+)
               (aref (gameboard-board game) row col))))
 
+; is a move square, diagonal, or not good? PORTABILITY SBCL is okay with
+; not returning enough VALUES for the "not good" condition, maybe
+; another LISP might complain about that, in which case add some nil to
+; pad to three the return VALUES
+(defun move-type (x1 y1 x2 y2)
+  (if (and (= x1 x2) (= y1 y2))
+    (values +move-other+)
+    (let ((dy (- y2 y1)))
+      (if (zerop dy)
+        (values +move-square+ (if (> x2 x1) 1 -1) 0)
+        (let ((dx (- x2 x1)))
+          (if (zerop dx)
+            (values +move-square+ 0 (if (> y2 y1) 1 -1))
+            (if (= 1 (abs (/ dy dx)))
+              (values +move-diagonal+
+                      (if (plusp dx) 1 -1)
+                      (if (plusp dy) 1 -1))
+              (values +move-other+))))))))
+
+; NOTE "moves" of zero for a second player turn is an illegal state; the
+; move count must be shared between pairs of turns. but that's a
+; validation detail for an as-yet-to-be-written board loader
 (defun print-board (game &optional (stream t))
   (let ((score (gameboard-score game)))
     (format stream "Marad 1 turn ~d moves ~d score ~d ~d~&"
--- marad.lisp
+++ marad.lisp
@@ -4,10 +4,10 @@
 ;   between Babylon and Isin. The city's main temple, a ziggurat, is
 ;   E-igi-kalama (House which is the eye of the land)"
 ;     -- https://en.wikipedia.org/wiki/Marad
+;
+; Yep, pretty much picked that at random.
 
 (in-package :marad)
-
-(block nil (setq *random-state* (make-random-state t)) (return))
 
 ; TODO ask SDL what the screen size is and then derive a bunch of the
 ; following constants from that
@@ -17,8 +17,8 @@
 ; for a NxN board
 (defconstant +board-cells+ 9)
 (defconstant +middle-cell+ 4)
-; don't let the board take up all the window (TODO probably the board
-; needs a border drawn around it)
+; don't let the board take up all the window. TODO also probably need
+; some space to show the score and the move count for the turn pair
 (defconstant +border-x+ 64)
 (defconstant +border-y+ 40)
 (defconstant +border-offx+ (/ +border-x+ 2))
@@ -33,19 +33,33 @@
 (defconstant +node-max-size+ 83)
 
 ; optional adjustments for from where lines or such get drawn from (the
-; background graph is drawn outside the window bounds)
+; background graph is drawn outside the window bounds, and the board
+; from somewhere inside)
 (defparameter *offset-x* 0)
 (defparameter *offset-y* 0)
 
+; probably lower this number on a slower system as a higher count can
+; eat up CPU figuring out how best to wire up the graph for the
+; background art. or just disable all that code.
 (defconstant +node-count+ 128)
 
 (defconstant +sdl-delay+ 128)   ; do not burn up the CPU too much
+
+; what cell in the board is selected
+(defstruct (boardcell (:type vector))
+  (x 0 :type fixnum)
+  (y 0 :type fixnum))
 
 ; bag of holding for "world" objects or whatever
 (defstruct (app (:type vector))
   rend      ; SDL renderer
   graph     ; background graph art thing
   slope     ; a slope function for the graph
+  images    ; textures, actually
+  board     ; game board
+  state     ; funcall of how to handle a click
+  src       ; boardcell of where a move is starting from
+  type      ; type of the src cell (King, Bishop, Pawn)
   cellwidth ; how big the board is
   cellsize  ; how big a board cell is
   cellxoff  ; where to start drawing the board from
@@ -126,6 +140,8 @@
          (boardsize (app-cellwidth app))
          (cellsize (app-cellsize app))
          (middle (* cellsize +middle-cell+))
+         (images (app-images app))
+         (game (app-board app))
          )
     (fill-rect renderer 0 0 boardsize boardsize)
     (sdl2:set-render-draw-color renderer 208 208 208 128) ; center cell skari
@@ -135,7 +151,17 @@
           (line-hori renderer +board-line-width+ 0 x boardsize)
           (line-vert renderer +board-line-width+ x 0 boardsize))
     ; TODO draw the board pieces uh can we get a bitmap for those?
-    ))
+    ; TODO with-gameboard is causing weird errors here dunno why
+    (with-board-pieces
+      (game board row col owner type)
+      ;(format t "PIECE ~a ~a ~a,~a ~a,~a~&" type owner) 
+      (sdl2:render-copy
+        renderer (aref images owner (1- type))
+        :dest-rect (sdl2:make-rect (+ *offset-x* (* cellsize col))
+                                   (+ *offset-y* (* cellsize row))
+                                   cellsize cellsize)))
+  ; TODO draw active? cell if that is enabled
+  ))
 
 (defun update (app)
   (let ((renderer (app-rend app)))
@@ -145,52 +171,138 @@
     (update-board renderer app)
     (sdl2:render-present renderer)))
 
+; pick the piece to move, if it's a valid one
+(defun movestate-set-active (app game row col)
+  (when-let ((cell (get-cell game row col)))
+    (let ((player (mod (gameboard-turn game) 2))) ; TODO cache this in app
+      (multiple-value-bind (type owner)
+                           (cell-details cell)
+        (when (= player owner)
+          (let ((src (app-src app)))
+            (psetf (app-type app) type
+                   (app-state app) #'movestate-finalize
+                   (boardcell-y src) row
+                   (boardcell-x src) col)))))))
+
+; pick where to move a selected piece to
+; have we got a valid move? if not, go back to prior state
+; may not need to store the "dst" point... actually might need that
+; elsewhere to log the move in some notation, or it could be a vector
+; for something something
+(defun movestate-finalize (app game dsty dstx)
+  (let* ((src (app-src app))
+         (srcx (boardcell-x src))
+         (srcy (boardcell-y src)))
+    (multiple-value-bind (mtype stepx stepy)
+                         (move-type srcx srcy dstx dsty)
+      (format t "MOVE? ~a ~a~&" mtype (app-type app))
+      (if (legal-move? mtype (app-type app))
+        (progn
+          (format t "VERILY MOVE ~a,~a ~a,~a ~a,~a~&" srcx srcy dstx dsty stepx stepy)
+          (do-move game srcx srcy stepx stepy)
+          ; TODO if current player gets cached in app must also toggle that
+          (next-turn game)
+          (setf (app-state app) #'movestate-set-active
+                (app-type app) +empty-cell+))
+        ; here we stay in "select a finalize cell" mode
+        (format t "MOVE MISMATCH ~a ~a~&" mtype (app-type app))
+        )
+      )))
+
+; reset the active (src) cell, probably from a click outside the board
+(defun movestate-reset (app)
+  (format t "STATE RESET~&")
+  (setf (app-type app) +empty-cell+
+        (app-state app) #'movestate-set-active))
+
+; did they click somewhere within the game board? states here are to
+; clear the source cell, set the source cell, and set the destination
+; cell, which if legal should trigger a move
+(defun click-to-board (app clickx clicky)
+  (let* ((cellsize (app-cellsize app))
+         (offx (app-cellxoff app))
+         (offy (app-cellyoff app))
+         ; TODO this should only be one value to subtract
+         (x (floor (/ (- clickx offx +border-offx+) cellsize)))
+         (y (floor (/ (- clicky offy +border-offy+) cellsize)))
+         (game (app-board app)))
+    (if (array-in-bounds-p (gameboard-board game) y x)
+      (progn
+        (format t "CELL? ~a,~a ~a,~a | ~a,~a -> ~a,~a~&" offx offy +border-offx+ +border-offy+ clicky clickx y x)
+        (funcall (app-state app) app game y x))
+      (movestate-reset app))
+    ))
+
 (defun eventually (app)
   (sdl2:with-event-loop
     (:method :poll)
+    ; TODO more keyboard control of the game might be nice to have
     (:keyup
       (:keysym keysym)
       (let ((scancode (sdl2:scancode-value keysym))
             (sym (sdl2:sym-value keysym))
             (mod-value (sdl2:mod-value keysym)))
         (cond
-          ((sdl2:scancode= scancode :scancode-q) (sdl2:push-event :quit)))
+          ((sdl2:scancode= scancode :scancode-escape) (movestate-reset app))
+          ; this is for easy debugging, probably replace with Q when ship
+          ((sdl2:scancode= scancode :scancode-q) (sdl2:push-event :quit))
+          )
         (format t "Key sym: ~a, code: ~a, mod: ~a~%" sym scancode mod-value)))
-; TODO do we need mouse motion?
-;   (:mousemotion
-;     (:x x :y y :xrel xrel :yrel yrel :state state)
-;     )
-; TODO steal a mouse and see other buttons get other button numbers
+    ; TODO steal a mouse and see other buttons get other button numbers.
+    ; in the meantime react to any old click
     (:mousebuttondown
       (:x x :y y :button button)
-      (format t "CLICK ~a,~a (~a)~&" x y button))
+      (click-to-board app x y))
     (:idle () (update app) (sdl2:delay +sdl-delay+))
     (:quit () t)))
 
-(defun world (&aux (app (make-app)))
-  (let ((graph (new-graph (+ +game-width+ +graph-more+)
+(defun load-image (renderer filename)
+  (let ((surface (load-bmp filename)))
+    (sdl2:set-color-key surface :true (sdl2:map-rgb (sdl2:surface-format surface) 255 255 255))
+      (let ((texture (sdl2:create-texture-from-surface renderer surface)))
+        (free-surface surface)
+        texture)))
+
+(defun load-images (app renderer)
+  (let ((images (make-array '(2 3))) surface)
+    (psetf (aref images 0 0) (load-image renderer "image/white-pawn.bmp")
+           (aref images 0 1) (load-image renderer "image/white-bishop.bmp")
+           (aref images 0 2) (load-image renderer "image/white-king.bmp")
+           (aref images 1 0) (load-image renderer "image/black-pawn.bmp")
+           (aref images 1 1) (load-image renderer "image/black-bishop.bmp")
+           (aref images 1 2) (load-image renderer "image/black-king.bmp"))
+    (setf (app-images app) images)))
+
+(defun new-world ()
+  (setq *random-state* (make-random-state t))
+  (let ((app (make-app))
+        (graph (new-graph (+ +game-width+ +graph-more+)
                           (+ +game-height+ +graph-more+) +node-count+)))
-    (setf (app-graph app) graph
+    (setf (app-board app) (new-gameboard +board-cells+)
+          (app-state app) #'movestate-set-active
+          (app-src app) (make-boardcell)
+          (app-graph app) graph
           (app-slope app) (new-clamped-slope
                             (nodeset-pop-min graph) +node-min-size+
-                            (nodeset-pop-max graph) +node-max-size+)))
-  (multiple-value-bind (total size xoff yoff)
-                       (square-size (- +game-width+ +border-x+)
-                                    (- +game-height+ +border-y+)
-                                    +board-cells+)
-    (setf (app-cellwidth app) total
-          (app-cellsize app) size
-          (app-cellxoff app) xoff
-          (app-cellyoff app) yoff))
-  app)
+                            (nodeset-pop-max graph) +node-max-size+))
+    (multiple-value-bind (total size xoff yoff)
+                         (square-size (- +game-width+ +border-x+)
+                                      (- +game-height+ +border-y+)
+                                      +board-cells+)
+      (setf (app-cellwidth app) total
+            (app-cellsize app) size
+            (app-cellxoff app) xoff
+            (app-cellyoff app) yoff))
+    app))
 
-(defun main (&aux (app (world)))
+(defun main (&aux (app (new-world)))
   (sdl2:with-init
     (:everything)
     (sdl2:with-window
       (win :title "Marad" :w +game-width+ :h +game-height+ :flags '(:shown))
       (sdl2:with-renderer
         (renderer win :flags '(:accelerated))
+        (load-images app renderer)
         (setf (app-rend app) renderer)
         (update app)
         (eventually app)))))
